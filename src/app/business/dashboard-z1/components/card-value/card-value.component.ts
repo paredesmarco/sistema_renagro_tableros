d6d44424e1dec1cdb031073daf5f119a41adcba4
Marco Paredes <mparedes.dev@gmail.com>
2025-09-01 17:38:55 -0500

    diseno titulo

--- src/app/business/dashboard-z1/components/card-value/card-value.component.ts
+++ src/app/business/dashboard-z1/components/card-value/card-value.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { Valor } from '../../interfaces/valor.interface';
-import { MatCard, MatCardTitle } from "@angular/material/card";
+import { MatCard, MatCardTitle, MatCardHeader, MatCardContent } from "@angular/material/card";
+import { MatDivider } from "@angular/material/divider";
 
 @Component({
   selector: 'app-card-value',
-  imports: [MatCard, MatCardTitle],
+  imports: [MatCard, MatCardTitle, MatCardHeader, MatDivider, MatCardContent],
   templateUrl: './card-value.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrl: './card-value.component.css'
