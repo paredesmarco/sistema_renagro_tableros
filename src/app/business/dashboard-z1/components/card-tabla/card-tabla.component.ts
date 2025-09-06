b358052a0142d4aa81c067bec3c580a137e20c6c
Marco Paredes <mparedes.dev@gmail.com>
2025-09-06 08:54:25 -0500

    CardTablaComponent

---
+++ src/app/business/dashboard-z1/components/card-tabla/card-tabla.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-tabla',
+  imports: [],
+  templateUrl: './card-tabla.component.html',
+  styleUrl: './card-tabla.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CardTablaComponent { }
